8fe3b88140ecc37d8e71180c8eda9002211628b8
STiGYFishh <STiGYFishh@users.noreply.github.com>
2018-06-08 20:10:39 +0100

    fixed key error

--- Joke_Score/Joke_Score.py
+++ Joke_Score/Joke_Score.py
@@ -174,11 +174,11 @@ class JokeScore:
 
         user = ctx.message.mentions[0]
         try:
-			poll_votes = self.votes[user.id]["incidents"][str(message_id)]["comment"]["votes"]
+			poll_votes = self.votes[user.id]["incidents"][str(message_id)]["votes"]
             self.votes[user.id]["incidents"].pop(str(message_id))
 			self.votes[user.id]["total"] -= poll_votes
+			
             await self.save_votes()
-
             await self.bot.say("Poll successfully removed!")
 
         except KeyError:
